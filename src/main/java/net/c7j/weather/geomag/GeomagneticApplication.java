f5f74835b9703aa683f50845098557c112602a1c
Illine <eekovtun@gmail.com>
2019-06-05 23:02:57 +0300

    init

---
+++ src/main/java/net/c7j/weather/geomag/GeomagneticApplication.java
@@ -0,0 +1,13 @@
+package net.c7j.weather.geomag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeomagneticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeomagneticApplication.class, args);
+    }
+
+}
