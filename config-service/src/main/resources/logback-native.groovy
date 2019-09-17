3a232d2372176c37721b4c76da9bc0a355434f43
Illine <eekovtun@gmail.com>
2019-09-17 11:27:20 +0300

    Logging & gitignore added logback-native.groovy to config-service added spring-boot-starter-actuator dependency to subproject of gradle changed /out/ to */out/* in gitignore removed spring-boot-starter-actuator dependency from geomagnetic-api project of gradle removed spring-boot-starter-actuator dependency from config-service project of gradle

---
+++ config-service/src/main/resources/logback-native.groovy
@@ -0,0 +1,11 @@
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+
+statusListener(NopStatusListener)
+
+appender("CONSOLE", ConsoleAppender) {
+    encoder(PatternLayoutEncoder) {
+        Pattern = "%d{ISO8601} [%thread] [%-5level] [%logger{36}] - %msg%n"
+    }
+}
+
+root(INFO, ["CONSOLE"])
