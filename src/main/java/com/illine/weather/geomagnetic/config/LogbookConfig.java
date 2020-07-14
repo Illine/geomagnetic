9353e55e2eeccac9ee93ed72f6b901fafa62523f
Evgeniya Kovtun <eekovtun@gmail.com>
2020-07-14 21:58:33 +0300

    GEO-27 (#86)
    added spring sleuth
    added jacocoTestCoverageVerification after tests
    configured logbook by sleuth
    configured jacoco
    removed debug and warn logging for logbook
    removed useless tests
    fixed .gitignore

--- src/main/java/com/illine/weather/geomagnetic/config/LogbookConfig.java
+++ src/main/java/com/illine/weather/geomagnetic/config/LogbookConfig.java
@@ -1,29 +1,25 @@
 package com.illine.weather.geomagnetic.config;
 
+import brave.Tracer;
 import com.illine.weather.geomagnetic.config.property.LogbookProperties;
 import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.zalando.logbook.Correlation;
-import org.zalando.logbook.HttpLogWriter;
-import org.zalando.logbook.Precorrelation;
+import org.zalando.logbook.*;
 
 import static com.illine.weather.geomagnetic.util.LogbackHelper.getLogger;
 
 @Configuration
 public class LogbookConfig {
 
-    private final LogbookProperties properties;
-
-    @Autowired
-    LogbookConfig(LogbookProperties properties) {
-        this.properties = properties;
+    @Bean
+    HttpLogWriter writer(LogbookProperties properties) {
+        return new GeomagneticHttpLogWriter(properties);
     }
 
     @Bean
-    HttpLogWriter writer() {
-        return new GeomagneticHttpLogWriter(properties);
+    public CorrelationId correlationId(Tracer tracer) {
+        return new SleuthCorrelationId(tracer);
     }
 
     public static class GeomagneticHttpLogWriter implements HttpLogWriter {
@@ -31,32 +27,36 @@ public class LogbookConfig {
         private final Logger logger;
 
         public GeomagneticHttpLogWriter(LogbookProperties properties) {
-            logger = getLogger(properties.getName(), properties.getLevel());
+            logger = getLogger(properties.getName());
         }
 
         @Override
         public boolean isActive() {
-            return logger.isDebugEnabled() || logger.isInfoEnabled() || logger.isWarnEnabled();
+            return logger.isInfoEnabled();
         }
 
         @Override
         public void write(Precorrelation precorrelation, String request) {
-            log(request);
+            logger.info(request);
         }
 
         @Override
         public void write(Correlation correlation, String response) {
-            log(response);
+            logger.info(response);
+        }
+    }
+
+    public static class SleuthCorrelationId implements CorrelationId {
+
+        private final Tracer tracer;
+
+        public SleuthCorrelationId(Tracer tracer) {
+            this.tracer = tracer;
         }
 
-        private void log(String message) {
-            if (logger.isDebugEnabled()) {
-                logger.debug(message);
-            } else if (logger.isInfoEnabled()) {
-                logger.info(message);
-            } else if (logger.isWarnEnabled()) {
-                logger.warn(message);
-            }
+        @Override
+        public String generate(HttpRequest request) {
+            return tracer.currentSpan().context().traceIdString();
         }
     }
 }
