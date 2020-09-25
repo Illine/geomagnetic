81616236fa33f2b47961c2190275d56ddd904596
Evgeniya Kovtun <eekovtun@gmail.com>
2020-09-25 19:12:06 +0300

    GEO-74 added api key security

--- src/test/java/ru/illine/weather/geomagnetic/rest/controller/AbstractControllerTest.java
+++ src/test/java/ru/illine/weather/geomagnetic/rest/controller/AbstractControllerTest.java
@@ -1,28 +1,29 @@
 package ru.illine.weather.geomagnetic.rest.controller;
 
-import ru.illine.weather.geomagnetic.model.dto.BaseResponse;
-import ru.illine.weather.geomagnetic.test.helper.UriHelper;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import ru.illine.weather.geomagnetic.config.property.ApiKeySecurityProperties;
+import ru.illine.weather.geomagnetic.model.dto.BaseResponse;
+import ru.illine.weather.geomagnetic.test.helper.UriHelper;
 
 import java.util.function.BiConsumer;
 
-import static ru.illine.weather.geomagnetic.test.helper.UriHelper.generateUri;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static ru.illine.weather.geomagnetic.test.helper.UriHelper.generateUri;
 
 abstract class AbstractControllerTest {
 
     @LocalServerPort
     protected int port;
+
+    @Autowired
+    protected ApiKeySecurityProperties apiKeyProperties;
 
     @Autowired
     protected TestRestTemplate restTemplate;
@@ -40,18 +41,24 @@ abstract class AbstractControllerTest {
         };
     }
 
-    protected <T> ResponseEntity<T> get(Class<T> response, String path) {
+    protected <T extends BaseResponse> ResponseEntity<T> get(Class<T> response, HttpHeaders headers, String path) {
         var uri = generateUri(path, port);
-        return restTemplate.getForEntity(uri, response);
+        return restTemplate.exchange(uri, HttpMethod.GET, new HttpEntity<>(headers), response);
     }
 
-    protected <T extends BaseResponse, R> ResponseEntity<T> patch(R request, String path) {
-        return exchange(request, HttpMethod.PATCH, path);
+    protected <T extends BaseResponse, R> ResponseEntity<T> patch(R request, HttpHeaders headers, String path) {
+        return exchange(request, headers, HttpMethod.PATCH, path);
     }
 
-    protected <T extends BaseResponse, R> ResponseEntity<T> exchange(R request, HttpMethod method, String path) {
+    protected <T extends BaseResponse, R> ResponseEntity<T> exchange(R request, HttpHeaders headers, HttpMethod method, String path) {
         var uri = UriHelper.generateUri(path, port);
-        return restTemplate.exchange(uri, method, new HttpEntity<>(request), new ParameterizedTypeReference<>() {
+        return restTemplate.exchange(uri, method, new HttpEntity<>(request, headers), new ParameterizedTypeReference<>() {
         });
+    }
+
+    protected HttpHeaders buildDefaultHeaders() {
+        var headers = new HttpHeaders();
+        headers.set(apiKeyProperties.getHeaderName(), apiKeyProperties.getAccessKeys().stream().findAny().orElseThrow());
+        return headers;
     }
 }
